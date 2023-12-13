4a7339cd08ff8585f904dca03f57cb6da5e21b70
Vaibhav  Nimesh <98076349+vaibhavnimesh1@users.noreply.github.com>
2023-12-13 23:12:57 +0530

    used bcrypt , jsw , schema methods

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "./env"
+  path: "./env",
 });
 
 connectDB()
+  .then(() =>
+    app.use(() => {
+      app.listen(process.env.PORT || 8000, () => {
+        console.log("server is running at port : ${process.env.PORT || 8000}");
+      });
+    })
+  )
+  .catch((err) => console.log("mongo connnection failed " + err));
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,16 @@
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
